ae14cc252ddfc58d0b69db3804aa5c296b268d97
dimitri-lev <dimitri.lev@protonmail.com>
2022-08-25 10:29:44 -0400

    init backend

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// connexion bdd
+mongoose
+  .connect(
+    `mongodb+srv://groupomania:Bf6HZOeWlvOixKTw@cluster0.73fvs.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// configuration cors
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+app.use(express.json());
+
+module.exports = app;
